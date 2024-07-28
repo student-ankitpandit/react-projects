3aff9dbcc46d46c739a6324f58cb9c62f3aef354
Student-ankitpandit <Student.ankitkumarpandit1221@gmail.in>
2024-07-28 18:54:29 +0530

    projects

--- counterproject/src/App.jsx
+++ counterproject/src/App.jsx
@@ -4,15 +4,37 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [counter, setCounter]  = useState(15)
+
+  //let counter = 15
+
+  const addValue = () => {
+    //counter = counter + 1
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1 )
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
+  
   return (
     <>
-      <h1>react ongoing</h1>
-      <h2>counter value: 5</h2>
-      <button>Add Value</button>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value {counter}</button> 
       <br />
-      <button>Remove Value</button>
+      <button
+      onClick={removeValue}
+      >remove value {counter}</button>
+      <p>footer: {counter}</p>
     </>
   )
 }
